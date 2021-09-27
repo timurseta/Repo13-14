47604522ba31a4ce484e9a97e840fee520080529
timurseta <timur_seta@hotmail.com>
2021-09-27 10:45:33 +0200

    final changes

--- ex01/scripts/index.js
+++ ex01/scripts/index.js
@@ -2,14 +2,17 @@ const openModal = document.getElementsByClassName('openModal');
 const modal = document.getElementsByClassName('modal');
 const close = document.getElementsByClassName('close');
 
-for (let i=0; i < openModal.length; i++) {
-    openModal[i].addEventListener('click',  () => {
-        modal[i].classList.add('on');
-    })
-}
+function myFunction() {
+  for (let i=0; i < openModal.length; i++) {
+      openModal[i].addEventListener('click',  () => {
+          modal[i].classList.add('on');
+      })
+  }
 
-for (let i=0; i < close.length; i++) {
-    close[i].addEventListener('click',  () => {
-        modal[i].classList.remove('on');
-    })
+  for (let i=0; i < close.length; i++) {
+      close[i].addEventListener('click',  () => {
+          modal[i].classList.remove('on');
+      })
+  }
 }
+myFunction();
